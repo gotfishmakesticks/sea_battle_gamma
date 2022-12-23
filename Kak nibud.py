3e90c5025c6795662537a3ff7f1fb5fbfb084658
Алкесей Мирнеков <prowlerrage@gmail.com>
2022-12-23 22:02:50 +0300

    АЛЕКСЕЙ МИРЕНКОВ

--- Kak nibud.py
+++ Kak nibud.py
@@ -2,15 +2,15 @@ from tkinter import *
 
 root = Tk()
 
-check = True
+check = True #переменная проверки, кто ходит
 class Field:
 
     def __init__(self, win, row, column, state):
         self.frame = Frame(win)
         self.frame.grid(row=row, column=column, padx=10, pady=10)
-
+        self.ID = column #айдишник фрейма
         # Словарь кнопок
-        btn_dict, btn_key = {}, 0
+        self.btn_dict, btn_key = {}, 0
 
         # Словарь букв для поля
         letters = {1: 'А', 2: 'Б', 3: 'В', 4: 'Г', 5: 'Д', 6: 'Е', 7: 'Ж', 8: 'З', 9: 'И', 10: 'К'}
@@ -27,28 +27,37 @@ class Field:
                         Label(self.frame, text=f'{letters[row]}').grid(row=row, column=column)
                     else:
                         # Создаём кнопку и помещаем её в словарь
-                        btn_dict[btn_key] = Button(self.frame, width=4, height=2, state=state, command=lambda btn=btn_key: self.attack(btn_dict[btn]))
-                        btn_dict[btn_key].grid(row=row, column=column)
+                        self.btn_dict[btn_key] = Button(self.frame, width=4, height=2, state=state, command=lambda btn=btn_key: self.attack(self.btn_dict[btn]))
+                        self.btn_dict[btn_key].grid(row=row, column=column)
                         btn_key += 1
 
     # Обработчик кнопки для пройденной атаки
     def attack(self, btn):
         global check
-        btn_key = 1
+        btn_key = 0
         btn['text'] = '*'
         btn['state'] = 'disabled'
-        print(self.frame["column"])
-        if check == True:
-            #print("Test")
-            for row in range(11):
-                for column in range(11):
-                    if row >= 1 & column >= 1:
-                        btn["state"] = "active"
-                        # btn_dict[btn_key].grid(row=row, column=column)
+        print(self.ID)
+        if check and self.ID == 1:
+            print("Test")
+            for row in range(1, 11):
+                for column in range(1, 11):
+                    f1.btn_dict[btn_key]["state"] = "active"
+                    self.btn_dict[btn_key]["state"] = "disabled"
+                    btn_key += 1
             check = False
-        else:
-            #print("Test Not")
+        elif check == False and self.ID == 0:
+            print("Test")
+            for row in range(1, 11):
+                for column in range(1, 11):
+                    f2.btn_dict[btn_key]["state"] = "active"
+                    self.btn_dict[btn_key]["state"] = "disabled"
+                    btn_key += 1
             check = True
+        #else:
+            #print("Test Not")
+            #check = True
+
 
 f1, f2 = Field(root, 0, 0, "disabled"), Field(root, 0, 1, "active")
 
